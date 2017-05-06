ba0fd93df3094a55576ab7cc388895eb594aa692
Matt Palmer <matt@silvercreekit.com>
2017-05-06 14:40:34 -0400

    Start implementing webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const Html = require('html-webpack-plugin');
+
+const paths = {
+  src: path.resolve('src'),
+};
+
+const base = {
+  context: paths.src,
+  entry: 'index.js',
+  resolve: {
+    modules: [
+      paths.src,
+      'node_modules',
+    ],
+  },
+  extensions: ['.js'],
+};
+
+const dev = {
+  devtool: 'eval-source-map',
+  plugins: [
+    new Html({
+      title: 'Tablr &bull; Dev',
+    }),
+  ],
+};
+
+module.exports = env => merge(base, dev);
