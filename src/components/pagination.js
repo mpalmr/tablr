23e7b5173e2aa6b4e7e8e55b1ebfc873347d3561
Matt Palmer <matt@silvercreekit.com>
2017-05-29 20:20:03 -0400

    Select menu

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,4 +1,4 @@
-function renderAdjacentButton(classModifier, label) {
+function adjacentButton(classModifier, label) {
   const buttonElement = document.createElement('button');
   buttonElement.appendChild(document.createTextNode(label));
   buttonElement.classList
@@ -6,9 +6,27 @@ function renderAdjacentButton(classModifier, label) {
   return buttonElement;
 }
 
-export default function renderPagination() {
+function pageSizeSelect(size, textBefore, textAfter) {
+  const selectElement = size.reduce((element, a) => {
+    const optionElement = document.createElement('option');
+    optionElement.value = a;
+    optionElement.appendChild(document.createTextNode(a));
+    element.appendChild(optionElement);
+    return element;
+  }, document.createElement('select'));
+
+  const labelElement = document.createElement('label');
+  if (textBefore) labelElement.appendChild(document.createTextNode(textBefore));
+  labelElement.appendChild(selectElement);
+  if (textAfter) labelElement.appendChild(document.createTextNode(textAfter));
+  return labelElement;
+}
+
+export default function pagination(options) {
   const containerElement = document.createElement('div');
-  containerElement.appendChild(renderAdjacentButton('previous', 'Previous'));
-  containerElement.appendChild(renderAdjacentButton('next', 'Next'));
+  containerElement.appendChild(adjacentButton('previous', 'Previous'));
+  containerElement.appendChild(adjacentButton('next', 'Next'));
+  containerElement.appendChild(pageSizeSelect(
+    options.size, options.textBefore, options.textAfter));
   return containerElement;
 }
